a836985ff83785b0b1045021517ca13c54f27589
13tom13 <13tom13@mail.ru>
2023-06-01 15:19:47 +0200

    implements Spring MVC and Embed Tomcat

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,45 +1,37 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
 
-    private final Gson gson = new Gson();
 
     public PostController(PostService service) {
         this.service = service;
     }
 
-    private void JSONresponse(Object o, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        response.getWriter().print(gson.toJson(o));
+    @GetMapping
+    public List<Post> all(){
+        return service.all();
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        final var data = service.all();
-        JSONresponse(data, response);
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        final var data = service.getById(id);
-        JSONresponse(data, response);
+    @PostMapping
+    public Post save(@RequestBody Post post){
+        return service.save(post);
     }
-
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        final var data = service.save(gson.fromJson(body, Post.class));
-        JSONresponse(data, response);
-    }
-
-    public void removeById(long id, HttpServletResponse response) throws IOException {
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        JSONresponse("Post with id " + id + " remove", response);
     }
 }
