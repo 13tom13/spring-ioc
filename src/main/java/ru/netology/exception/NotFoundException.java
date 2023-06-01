6463e75db10ac2562106f03447da6d49f1b65cff
13tom13 <13tom13@mail.ru>
2023-06-01 16:58:24 +0200

    add removed functional

--- src/main/java/ru/netology/exception/NotFoundException.java
+++ src/main/java/ru/netology/exception/NotFoundException.java
@@ -1,7 +1,13 @@
 package ru.netology.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-  public NotFoundException() {
+
+  public NotFoundException(long id) {
+    super("Post with id " + id + " not found");
   }
 
   public NotFoundException(String message) {
